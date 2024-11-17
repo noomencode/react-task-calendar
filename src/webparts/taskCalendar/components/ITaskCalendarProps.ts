e51d2abf838600d52c6ce065986ac28afc334744
sinoom <sinoom@ttu.ee>
2024-11-17 16:36:02 +0200

    initial commit

---
+++ src/webparts/taskCalendar/components/ITaskCalendarProps.ts
@@ -0,0 +1,7 @@
+export interface ITaskCalendarProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/taskCalendar/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ITaskCalendarWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'TaskCalendarWebPartStrings' {
+  const strings: ITaskCalendarWebPartStrings;
+  export = strings;
+}
